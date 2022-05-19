23231fc3f17b295cad9b376813faffee9b04ffde
hans <hansleonel@gmail.com>
2022-05-19 01:21:35 -0500

    add update code to better performance

---
+++ ios/build/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/GeneratedPluginRegistrant.d
@@ -0,0 +1,20 @@
+dependencies: \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/build/Debug-iphoneos/Flutter.framework/Modules/module.modulemap \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterViewController.h \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterEngineGroup.h \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterEngine.h \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterDartProject.h \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterCallbackCache.h \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterTexture.h \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterPlatformViews.h \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterCodecs.h \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterChannels.h \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterPlugin.h \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterMacros.h \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/build/Debug-iphoneos/Flutter.framework/Headers/Flutter.h
---
+++ ios/build/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/hans/Desktop/Flutter/Prime\ Applications/movieApp/ios/build/Runner.build/Debug-iphoneos/Runner.build/DerivedSources/Runner_vers.c
